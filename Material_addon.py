95e56f07ab0f96b4ae2184a7437d6412c64a9697
Kon4ka <62174872+Kon4ka@users.noreply.github.com>
2024-08-01 16:50:29 +0300

    Сheck nodes and grpups separately

--- Material_addon.py
+++ Material_addon.py
@@ -11,30 +11,22 @@ def check_node_usage(node_tree):
 
 
 
-# Получаем все материалы в сцене
-materials = bpy.data.materials
 os.system('cls')
-# Проверяем каждый материал на наличие нод
+materials = bpy.data.materials
+print("=== Start work with Materials ===")
 for material in materials:
     if material.use_nodes:
         print(f"Material: {material.name} uses nodes")
-        for node in material.node_tree.nodes:
-            
-            # Проверяем, используется ли нода (подключены ли её выходы)
-            is_used = any(output.is_linked for output in node.outputs) or any(input.is_linked for input in node.inputs)
-            if is_used:
-#                print(f"  Node: {node.name}, Type: {node.type} is used")
-                # Если нода является группой, обрабатываем её отдельно
-                pass
-            else:
-                print(f"  Node: {node.name}, Type: {node.type} is not used at material "
-                      f"{material.name}") 
-            
+        unused_nodes = check_node_usage(material.node_tree)
+        for node in unused_nodes:
+            print(f"  Node: {node.name}, Type: {node.type} is not used in material {material.name}")
     else:
         print(f"Material: {material.name} does not use nodes")
         
+print("=== Start work with Node Groups ===")
 for node_group in bpy.data.node_groups:
-#        print(f"Node Group: {node_group.name}")
-        unused_nodes = check_node_usage(node_group)
-        for node in unused_nodes:
-            print(f"  Node: {node.name}, Type: {node.type} is not used in node group {node_group.name}")
+        unused_nodes_at_groups = check_node_usage(node_group)
+        if unused_nodes_at_groups:
+            print(f"Node Group: {node_group.name}")
+            for node in unused_nodes_at_groups:
+                print(f"Node: {node.name}, Type: {node.type} is not used in node group {node_group.name}")
