c5ffa36d18cde6d0b774f7f2052c9108592ccfd2
Kon4ka <62174872+Kon4ka@users.noreply.github.com>
2024-08-01 15:36:20 +0300

    material trees

--- Material_addon.py
+++ Material_addon.py
@@ -1,14 +1,9 @@
 import bpy
 
-for ob in bpy.data.objects:
-    print (ob.name)
-    try:
-        print (ob.name)
-        material = ob.active_material
-        print(material)
-        
-        material_slots = ob.material_slots
-        for material in material_slots
-            print(material1)
-    except:
-        print("Wrong")
+# Получаем все материалы в сцене
+materials = bpy.data.materials
+
+# Выводим список всех материалов
+for material in materials:
+    print(f"Material: {material.name}")
+
